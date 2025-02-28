cfceae8c570ecfa97435a2d57a428d87fea258dd
Shahzaib Khan <drshahzeb47@gmail.com>
2025-02-28 16:38:46 +0500

    main context fixes

--- src/context/MainContext.tsx
+++ src/context/MainContext.tsx
@@ -1,71 +1,71 @@
-import { createContext, useEffect, useRef, useState } from "react";
-import { configFiles } from "../config/config";
-import emailjs from "@emailjs/browser";
+// import { createContext, useEffect, useRef, useState } from "react";
+// // import { configFiles } from "../config/config";
+// // import emailjs from "@emailjs/browser";
 
-export const MainContext = createContext();
+// export const MainContext = createContext({});
 
-export const MainContextProvider = ({ children }) => {
-  // <----------------- States -------------------->
+// export const MainContextProvider = ({ children }) => {
+//   // <----------------- States -------------------->
 
-  const [navMenu, setNavMenu] = useState(false);
-  const [messageSent, setMessageSent] = useState("");
-  const [underline, setUnderline] = useState("home");
-  // <------------------------------ References (useRef etc.) -------------------->
+//   const [navMenu, setNavMenu] = useState(false);
+//   const [messageSent, setMessageSent] = useState("");
+//   const [underline, setUnderline] = useState("home");
+//   // <------------------------------ References (useRef etc.) -------------------->
 
-  const formRef = useRef(null);
+//   const formRef = useRef(null);
 
-  // <---------------------------- Methods and Functions -------------------->
+//   // <---------------------------- Methods and Functions -------------------->
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-    const refValue = formRef.current.elements;
-    const selectedElem = Array.from(refValue).filter(
-      (elem) => elem.tagName === "INPUT" || elem.tagName === "TEXTAREA"
-    );
-    const isEmpty = selectedElem.some((val) => val.value.trim() === "");
-    const minLen = selectedElem.some((val) => val.value.length <= 3);
-    if (isEmpty) {
-      setMessageSent("All fields required");
-      return;
-    } else if (minLen) {
-      setMessageSent("Length is less than 4 characters");
-      return;
-    }
-    setMessageSent("Sending...");
+//   // const sendEmail = (e) => {
+//   //   e.preventDefault();
+//   //   const refValue = formRef.current.elements;
+//   //   const selectedElem = Array.from(refValue).filter(
+//   //     (elem) => elem.tagName === "INPUT" || elem.tagName === "TEXTAREA"
+//   //   );
+//   //   const isEmpty = selectedElem.some((val) => val.value.trim() === "");
+//   //   const minLen = selectedElem.some((val) => val.value.length <= 3);
+//   //   if (isEmpty) {
+//   //     setMessageSent("All fields required");
+//   //     return;
+//   //   } else if (minLen) {
+//   //     setMessageSent("Length is less than 4 characters");
+//   //     return;
+//   //   }
+//   //   setMessageSent("Sending...");
 
-    emailjs
-      .sendForm(
-        configFiles.emailJsServiceKey,
-        configFiles.emailJsTemplateKey,
-        formRef.current,
-        {
-          publicKey: configFiles.emailJsPublicKey,
-        }
-      )
-      .then(
-        () => {
-          setMessageSent("Message sent !");
-          formRef.current.reset();
-        },
-        (err) => {
-          console.log("Error occured ! ", err);
-          setMessageSent("Message not sent !");
-          formRef.current.reset();
-        }
-      );
-  };
+//   //   emailjs
+//   //     .sendForm(
+//   //       configFiles.emailJsServiceKey,
+//   //       configFiles.emailJsTemplateKey,
+//   //       formRef.current,
+//   //       {
+//   //         publicKey: configFiles.emailJsPublicKey,
+//   //       }
+//   //     )
+//   //     .then(
+//   //       () => {
+//   //         setMessageSent("Message sent !");
+//   //         formRef.current.reset();
+//   //       },
+//   //       (err) => {
+//   //         console.log("Error occured ! ", err);
+//   //         setMessageSent("Message not sent !");
+//   //         formRef.current.reset();
+//   //       }
+//   //     );
+//   // };
 
-  // <--------------------- Rendering Area (useEffects etc.)-------------------->
+//   // <--------------------- Rendering Area (useEffects etc.)-------------------->
 
-  // <----------------- Context Values -------------->
-  const contextValues = {
-    navMenu,
-    setNavMenu,
-  };
+//   // <----------------- Context Values -------------->
+//   const contextValues = {
+//     navMenu,
+//     setNavMenu,
+//   };
 
-  return (
-    <MainContext.Provider value={contextValues}>
-      {children}
-    </MainContext.Provider>
-  );
-};
+//   return (
+//     <MainContext.Provider value={contextValues}>
+//       {children}
+//     </MainContext.Provider>
+//   );
+// };
